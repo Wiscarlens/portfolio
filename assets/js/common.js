f4d4168c38a4d1b782464e573e169093721ebf2f
josewebdev2000 <josewebdev@proton.me>
2023-10-06 12:43:21 -0400

    Modified code to focus on front-end only

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,5 @@
 // Define common data to be shared throughout the project
 const coreURL = "http://localhost/artist-portfolio-photo-gallery/";
 const mainPageLink = "http://localhost/artist-portfolio-photo-gallery/index.html";
-const portfolioDetailsBackEndProcessorLink = "http://localhost/artist-portfolio-photo-gallery/portfolio.php";
-const contactBackEndProcessorLink = "http://localhost/artist-portfolio-photo-gallery/contact.php";
+const portfolioDetailsBackEndProcessorLink = ""; // Link to Java/Python Server Route that will deliver information about Portfolio Details
+const contactBackEndProcessorLink = ""; // Link to Java/Python Server Route that will handle automatic email sending
